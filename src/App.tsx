30f10f32a1acb5382f8e3183b3803e7edf3cb0cb
Matt Stvartak <hello@mattstvartak.com>
2023-03-11 10:14:46 -0500

    Revert to before AWS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import 'antd/dist/reset.css'
 import './App.css'
 import Orders from './pages/Orders'
-import { Amplify } from 'aws-amplify';
-import awsmobile from './aws-exports';
-
-Amplify.configure(awsmobile);
 
 function App() {
   return (
