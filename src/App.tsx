d4fe6a5943fee7341f94df96469815a86134442b
Matt Stvartak <hello@mattstvartak.com>
2023-03-11 10:07:18 -0500

    Fixed aws-exports import.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import 'antd/dist/reset.css'
 import './App.css'
 import Orders from './pages/Orders'
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+import awsmobile from './aws-exports';
 
-Amplify.configure(awsExports);
+Amplify.configure(awsmobile);
 
 function App() {
   return (
