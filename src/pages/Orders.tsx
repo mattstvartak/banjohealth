f77798f60257eff4a080bd77220051f48254ec52
Matt Stvartak <hello@mattstvartak.com>
2023-03-11 09:19:21 -0500

    Removed starter content. Added Orders  page.

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    margin: 0 auto;
+    width: 800px;
+    padding: 20px;
+`
+const Orders = () => {
+
+    return (
+        <Wrapper>
+            Hello, world.
+        </Wrapper>
+    )
+}
+
+export default Orders;
