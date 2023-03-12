09c303b38d4ac2bcf98c9baf598f9f5774c9a520
Matt Stvartak <hello@mattstvartak.com>
2023-03-12 12:06:42 -0400

    Updated configs to get Vite working with Amplify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import resolve from "@rollup/plugin-node-resolve";
+import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {global: {},}
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: false,
+      dedupe: ["antd"]
+    }),
+    react(),
+  ],
 })
